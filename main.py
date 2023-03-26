0cce3091c17ae1eda91c9ab3cfb1e90535f594f3
dwjmoore <>
2023-03-26 15:36:40 +0000

    created date using arguments

---
+++ main.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+birthday = datetime(1980, 3, 30, 3, 30)
+
+print(birthday.year)
+print(birthday.month)
+print(birthday.day)
+print(birthday.hour)
+print(birthday.weekday())
